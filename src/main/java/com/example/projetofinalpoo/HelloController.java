1ce7746fda631b699c11cc41333b83042d2b974a
vinicius <viniciusmgomes2005@hotmail.com>
2023-12-15 19:50:23 -0300

    Mudando para nova tela, e mudando layout da tela

--- src/main/java/com/example/projetofinalpoo/HelloController.java
+++ src/main/java/com/example/projetofinalpoo/HelloController.java
@@ -2,7 +2,13 @@ package com.example.projetofinalpoo;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class HelloController {
 
@@ -10,8 +16,12 @@ public class HelloController {
     private Button btnEntrar;
 
     @FXML
-    void onHelloButtonClick(ActionEvent event) {
-        System.out.println("Botão Clicado");
+    void onHelloButtonClick(ActionEvent event) throws IOException {
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloController.class.getResource("telaInicial.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 900, 700);
+        stage.setScene(scene);
+        stage.show();
     }
 
 }
