f5e532f1b2b1124655c9fd15615a00fc686fad3d
JoaoGuilhermeMA <joomoreira36@gmail.com>
2023-12-15 16:37:38 -0300

    excluindo testando

--- src/main/java/com/example/projetofinalpoo/HelloApplication.java
+++ src/main/java/com/example/projetofinalpoo/HelloApplication.java
@@ -13,7 +13,6 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         // conectando no banco de dados
-        //testando
         Conexao.conectar();
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
