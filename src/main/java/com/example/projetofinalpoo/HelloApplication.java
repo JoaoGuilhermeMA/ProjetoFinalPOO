4c1c189c805c4085a9f5a6abaf619c30ce742788
vinicius <viniciusmgomes2005@hotmail.com>
2023-12-15 17:33:35 -0300

    Alterando layout da página

--- src/main/java/com/example/projetofinalpoo/HelloApplication.java
+++ src/main/java/com/example/projetofinalpoo/HelloApplication.java
@@ -12,9 +12,9 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-
         Conexao.conectar();
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 900, 700);
+
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
