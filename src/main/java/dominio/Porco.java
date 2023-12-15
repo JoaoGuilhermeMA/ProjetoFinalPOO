d1b59da849a5d21b52c21f4660e3fbf8f00fac2f
JoaoGuilhermeMA <joomoreira36@gmail.com>
2023-12-15 16:49:02 -0300

    criando a classe porco

---
+++ src/main/java/dominio/Porco.java
@@ -0,0 +1,88 @@
+public class Porco {
+    private int idAnimal;
+    private String sexo;
+    private String peso;
+    private int idade;
+    private String raca;
+    private int idVacina;
+    private int idCuidador;
+    private int idVivedouro;
+    private boolean vendido;
+
+    // Construtor vazio
+    public Porco() {
+    }
+
+    // Getters e Setters
+    public int getIdAnimal() {
+        return idAnimal;
+    }
+
+    public void setIdAnimal(int idAnimal) {
+        this.idAnimal = idAnimal;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getPeso() {
+        return peso;
+    }
+
+    public void setPeso(String peso) {
+        this.peso = peso;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getRaca() {
+        return raca;
+    }
+
+    public void setRaca(String raca) {
+        this.raca = raca;
+    }
+
+    public int getIdVacina() {
+        return idVacina;
+    }
+
+    public void setIdVacina(int idVacina) {
+        this.idVacina = idVacina;
+    }
+
+    public int getIdCuidador() {
+        return idCuidador;
+    }
+
+    public void setIdCuidador(int idCuidador) {
+        this.idCuidador = idCuidador;
+    }
+
+    public int getIdVivedouro() {
+        return idVivedouro;
+    }
+
+    public void setIdVivedouro(int idVivedouro) {
+        this.idVivedouro = idVivedouro;
+    }
+
+    public boolean isVendido() {
+        return vendido;
+    }
+
+    public void setVendido(boolean vendido) {
+        this.vendido = vendido;
+    }
+}
