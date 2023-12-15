5d0717c6b0ba29168956d834d56bf3540de0b844
vinicius <viniciusmgomes2005@hotmail.com>
2023-12-15 20:52:23 -0300

    Alterando as telas e criando novas telas

--- src/main/java/com/example/projetofinalpoo/TelaInicialController.java
+++ src/main/java/com/example/projetofinalpoo/TelaInicialController.java
@@ -20,4 +20,13 @@ public class TelaInicialController {
         stage.setScene(scene);
         stage.show();
     }
+
+    @FXML
+    void telaMedicacao(ActionEvent event) throws IOException {
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloController.class.getResource("telaMedicacao.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 900, 700);
+        stage.setScene(scene);
+        stage.show();
+    }
 }
---
+++ src/main/java/com/example/projetofinalpoo/TelaMedicacaoController.java
@@ -0,0 +1,5 @@
+package com.example.projetofinalpoo;
+
+public class TelaMedicacaoController {
+
+}
