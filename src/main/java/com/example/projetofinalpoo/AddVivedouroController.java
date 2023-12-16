eab88a481947723f7fd4fd91cd672daa83289f67
JoaoGuilhermeMA <joomoreira36@gmail.com>
2023-12-16 20:16:22 -0300

    mostrando dados na table view de viveodouro

--- src/main/java/com/example/projetofinalpoo/AddVivedouroController.java
+++ src/main/java/com/example/projetofinalpoo/AddVivedouroController.java
@@ -1,4 +1,37 @@
 package com.example.projetofinalpoo;
 
+import dominio.Cuidador;
+import dominio.Vivedouro;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.event.ActionEvent;
+import persistencia.Conexao;
+import persistencia.CuidadoDAO;
+import persistencia.VivedouroDAO;
+
+import java.sql.SQLException;
+
 public class AddVivedouroController {
+    @FXML
+    private TextField campoTamanho;
+
+    @FXML
+    private TextField campoVivedouro;
+
+    @FXML
+    private Button salvarDados;
+
+    @FXML
+    void onClick(ActionEvent event) throws SQLException {
+        Conexao conexao = new Conexao();
+        Vivedouro vivedouro = new Vivedouro();
+        VivedouroDAO vivDAO = new VivedouroDAO(conexao.getConexao());
+        vivedouro.setVivedouro(campoVivedouro.getText());
+        vivedouro.setTamanho(campoTamanho.getText());
+        vivDAO.adicionarVivedouro(vivedouro);
+        conexao.fecharConexao();
+    }
+
 }
+
--- src/main/java/com/example/projetofinalpoo/VivedouroController.java
+++ src/main/java/com/example/projetofinalpoo/VivedouroController.java
@@ -1,5 +1,7 @@
 package com.example.projetofinalpoo;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -7,14 +9,24 @@ import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
+import persistencia.Conexao;
+import persistencia.VivedouroDAO;
+import dominio.Vivedouro;
+import javafx.scene.control.TableView;
 
 import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
 
 public class VivedouroController {
 
     @FXML
     private Button adicionar;
+
+    @FXML
+    private TableView<Vivedouro> tabelaVivedouro;
 
     @FXML
     private TableColumn<?, ?> tamanho;
@@ -24,6 +36,30 @@ public class VivedouroController {
 
     @FXML
     private TableColumn<?, ?> idVivedouro;
+
+    Conexao conexao = new Conexao();
+    VivedouroDAO vivedouroDAO = new VivedouroDAO(conexao.getConexao());
+
+    public void initialize() {
+        configurarTableView();
+        carregarDadosNaTableView();
+    }
+
+    private void configurarTableView() {
+        idVivedouro.setCellValueFactory(new PropertyValueFactory<>("idVivedouro"));
+        tipoVivedouro.setCellValueFactory(new PropertyValueFactory<>("vivedouro"));
+        tamanho.setCellValueFactory(new PropertyValueFactory<>("tamanho"));
+    }
+
+    private void carregarDadosNaTableView() {
+        try {
+            List<Vivedouro> listaVivedouros = vivedouroDAO.listarVivedouros();
+            ObservableList<Vivedouro> listaObservable = FXCollections.observableArrayList(listaVivedouros);
+            tabelaVivedouro.setItems(listaObservable);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 
     @FXML
     void adicionarVivedouro(ActionEvent event) throws IOException {
