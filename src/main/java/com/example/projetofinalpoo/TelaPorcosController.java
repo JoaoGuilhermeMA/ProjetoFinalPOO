fc2a32fa935ae51822d1d75084a3599624b05a5e
vinicius <viniciusmgomes2005@hotmail.com>
2023-12-15 21:46:12 -0300

    Criando o restante das telas

--- src/main/java/com/example/projetofinalpoo/TelaPorcosController.java
+++ src/main/java/com/example/projetofinalpoo/TelaPorcosController.java
@@ -1,6 +1,7 @@
 package com.example.projetofinalpoo;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Scene;
@@ -9,7 +10,7 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class TelaPorcosController {
-
+    @FXML
     void adicionarPorco(ActionEvent event) throws IOException {
         Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         FXMLLoader fxmlLoader = new FXMLLoader(HelloController.class.getResource("addPorco.fxml"));
