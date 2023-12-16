e87697af7bdce62b19b75f97a14144d87bc9b4cd
vinicius <viniciusmgomes2005@hotmail.com>
2023-12-15 21:35:53 -0300

    Criando tela pra mostrar porcos e cadastrar funcionario

--- src/main/java/com/example/projetofinalpoo/TelaPorcosController.java
+++ src/main/java/com/example/projetofinalpoo/TelaPorcosController.java
@@ -1,6 +1,20 @@
 package com.example.projetofinalpoo;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class TelaPorcosController {
 
-
+    void adicionarPorco(ActionEvent event) throws IOException {
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloController.class.getResource("addPorco.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1000, 700);
+        stage.setScene(scene);
+        stage.show();
+    }
 }
