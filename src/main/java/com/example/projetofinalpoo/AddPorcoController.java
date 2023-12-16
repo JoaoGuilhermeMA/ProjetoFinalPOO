659dcb933685263033b212e3bfd584fec80a4caa
vinicius <viniciusmgomes2005@hotmail.com>
2023-12-16 20:10:52 -0300

    Alterando tela de adicionar porco e criando estilo para as telas de adicionar

--- src/main/java/com/example/projetofinalpoo/AddPorcoController.java
+++ src/main/java/com/example/projetofinalpoo/AddPorcoController.java
@@ -1,4 +1,22 @@
 package com.example.projetofinalpoo;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class AddPorcoController {
+
+    @FXML
+    void voltarPorco(ActionEvent event) throws IOException {
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloController.class.getResource("porco.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1000, 700);
+        stage.setScene(scene);
+        stage.show();
+    }
 }
